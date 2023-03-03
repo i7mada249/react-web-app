7480deea66f4cf54590c5205ee752ed19dfaee43
i7mada249 <i7mada249@outlook.com>
2023-03-03 09:30:43 -0700

    update

--- src/comp/brand/Brand.jsx
+++ src/comp/brand/Brand.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import './Brand.css';
-
+import {google, slack, dropbox, attlasian, shopify, atlassian} from './import'
 
 const Brand = () => {
   return (
-    <div>Brand</div>
+    <div className='brand section__padding'>
+      <div>
+        <img src={google} alt="google" />
+      </div>
+      <div>
+        <img src={slack} alt="slack" />
+      </div>
+      <div>
+        <img src={atlassian} alt="atlassian" />
+      </div>
+      <div>
+        <img src={dropbox} alt="dropbox" />
+      </div>
+      <div>
+        <img src={shopify} alt="shopify" />
+      </div>
+    </div>
   )
 }
 
--- src/cont/whatis/Whatis.jsx
+++ src/cont/whatis/Whatis.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Feature } from '../../comp';
 import './whatis.css';
 
 const whatis = () => {
   return (
-    <div>whatis</div>
+    <div className='whatis section__margin' id='wgpt3'>
+      <div className='whatgpt3-feature'>
+        <Feature />
+      </div>
+      <div className='whatgpt3-heading'>
+        <h1 className='gradient-text'>The possibilities are beyond your imagination</h1>
+        <p>Explore the library</p>
+      </div>
+    </div>
   )
 }
 
